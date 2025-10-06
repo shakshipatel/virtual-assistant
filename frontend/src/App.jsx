ed92f7a490d1fc589da2452f86d2b80f2769e290
Shakshi Patel <shakship003@gmail.com>
2025-10-06 23:15:55 +0530

    virtual assistant project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        
+      </Routes>
+    </div>
+  )
+}
+
+export default App
